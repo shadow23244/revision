582e1fce2531db747578ffb5d175063a6c5602e2
kamle <kamle@DESKTOP-LET1D06>
2023-07-17 14:47:26 +0530

    new waits

--- src/waits/waits_all.java
+++ src/waits/waits_all.java
@@ -1,5 +1,38 @@
 package waits;
 
-public class waits_all {
+import java.util.NoSuchElementException;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class waits_all 
+{
+	public static void main(String[] args) 
+	{
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--remote-allow-origins=*"); // used because newer chrome versions need this
+		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver(options); 
+		
+		
+		WebDriverWait wait1=new WebDriverWait(driver, 20);
+		wait1.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("https://www.facebook.com")));
+		
+		
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		
+		Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)							
+				.withTimeout(30,TimeUnit.SECONDS)
+				.pollingEvery(5, TimeUnit.SECONDS) 			
+				.ignoring(NoSuchElementException.class);
+		
+	}
 
 }
